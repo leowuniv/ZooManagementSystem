cac9dc9a6503bb671f133c019e716877b1b5b358
Leo <leowuniv@gmail.com>
2025-03-30 23:08:15 -0700

    Update ManagementSystem.py

--- ManagementSystem.py
+++ ManagementSystem.py
@@ -2,4 +2,8 @@ import heapq
 
 class Manage:
   
-e
+# **CARE FACILITIES** cannot be used by multiple animals at the same time (can implement as boolean value) --> not a class some data structure
+
+# if animal in the facility then NOT AVAILABLE HAVE TO WAIT THE TURN (hash table and binary tree)
+
+# facility can exist free floating does NOT HAVE TO EXIST IN ANY DATA STRUCTURE

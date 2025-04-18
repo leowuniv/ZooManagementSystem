c0dafe22acc1999a7ec8af3593fd05d8f51dce89
Ryan Sands <sandsryanj@gmail.com>
2025-04-18 10:50:32 -0700

    skeleton for demo

---
+++ main.py
@@ -0,0 +1,11 @@
+import ManagementSystem as ms
+
+if __name__ == "__main__":
+    a1 = ms.Animal('Bob', 'Tiger', 1)
+    a2 = ms.Animal()
+    # create 8 more animals "Populate your structures with at least 10 sample animals."
+    basic_care = ms.CareFacility(1, 3)
+    advanced_care = ms.CareFacility(4, 7)
+    intensive_care = ms.CareFacility(8, 10)
+
+    # do stuff here
